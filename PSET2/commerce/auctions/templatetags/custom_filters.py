3e65f2edcdf70d4c635756159c87306ec9bff2f5
Pirat102 <patryk.pirowski@gmail.com>
2024-12-11 23:06:45 +0100

    organize folders

--- PSET2/commerce/auctions/templatetags/custom_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-import re
-
-register = template.Library()
-
-@register.filter
-def first_sentence(value):
-    sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', value)
-    return sentences[0] if sentences else value
--- PSET4/project4/network/utils.py
+++
@@ -1,8 +0,0 @@
-from django.core.paginator import Paginator
-
-def paginate_queryset(request, queryset, per_page=10):
-    paginator = Paginator(queryset, per_page)
-    page_number = request.GET.get("page")
-    page = paginator.get_page(page_number)
-    return paginator, page
-    
